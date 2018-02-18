ab5fada041b5501a93c4fcb3fece937156e2e570
Roger Ehmpke <rehmpke@gmail.com>
2018-02-18 12:08:58 -0600

    First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Pool and Spa Inventory</h1>
+        </header>
+        <p className="App-intro">
+          Our family pool and spa chemical inventory system.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
