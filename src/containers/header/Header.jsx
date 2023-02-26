433385e59f3b2f47e0932bc7407f4a89c8ed6b89
Roger Ehmpke <rehmpke@gmail.com>
2023-02-26 00:35:43 -0600

    adjusting header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './header.css';
 function Header() {
   return (
-    <div className="app__header">
+    <div className="app__header section__padding" id="home">
+      <div className="app__header-content">
         <h1 className="app__header-title">Pool and Spa Inventory</h1>
         <p>Our family pool and spa chemical inventory system.</p>
+      </div>
     </div>
   )
 }
